3ac6b68abd79127b5d625163d81cced67e93b5d0
kojirakojira <amukas_maoto@yahoo.co.jp>
2022-10-22 05:02:07 +0900

    個体値が0-15の範囲であることを確認するvalidateを追加

--- src/main/java/jp/brainjuice/pokego/web/form/req/research/cp/CpRankRequest.java
+++ src/main/java/jp/brainjuice/pokego/web/form/req/research/cp/CpRankRequest.java
@@ -1,5 +1,7 @@
 package jp.brainjuice.pokego.web.form.req.research.cp;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 
@@ -16,10 +18,16 @@ public class CpRankRequest extends Request {
 //	private String id;
 //	private String name;
 	@NotNull
+	@Min(0)
+	@Max(15)
 	private Integer iva;
 	@NotNull
+	@Min(0)
+	@Max(15)
 	private Integer ivd;
 	@NotNull
+	@Min(0)
+	@Max(15)
 	private Integer ivh;
 	@Null
 	private String pl;
