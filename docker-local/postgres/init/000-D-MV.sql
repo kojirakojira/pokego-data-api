af06654ee30b8bfe1f7b57d44c530dd2f2a26c62
kojirakojira <ryo.sakuma.biz@gmail.com>
2026-05-31 16:19:54 +0900

    データを別リポジトリに切り出し

--- docker-local/postgres/init/000-D-MV.sql
+++
@@ -1,9 +0,0 @@
-/** go_pokedexの削除 */
-DROP MATERIALIZED VIEW IF EXISTS go_pokedex;
-
-/** too_strongの削除 */
-DROP MATERIALIZED VIEW IF EXISTS too_strong;
-
-/** multiplierの削除 */
-DROP MATERIALIZED VIEW IF EXISTS cp_multiplier;
-
--- docker-local/postgres/init/811-C-admin_comment.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS admin_comment (
-  comment_id integer,
-  comment varchar not null,
-  PRIMARY KEY(comment_id)
-);
